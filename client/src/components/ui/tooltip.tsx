8299966ea866c3cf5050e50251270290f35c3e02
xBrook2908 <bm29081998@gmail.com>
2025-09-29 19:31:37 +0300

    Create tooltip.tsx

---
+++ client/src/components/ui/tooltip.tsx
@@ -0,0 +1,9 @@
+import {
+  Tooltip as TooltipPrimitive,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip"
+
+export const Tooltip = TooltipPrimitive
+export { TooltipTrigger, TooltipContent, TooltipProvider }
