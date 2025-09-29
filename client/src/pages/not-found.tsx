64f6e319c5ae161ec6478a9f8aac2eac93398f14
xBrook2908 <bm29081998@gmail.com>
2025-09-29 19:19:38 +0300

    Create not-found.tsx

---
+++ client/src/pages/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+    </div>
+  )
+}
