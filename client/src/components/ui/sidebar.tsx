6bb24f09b59749c34040b5866f6dea6402e816eb
xBrook2908 <bm29081998@gmail.com>
2025-09-29 22:43:06 +0300

    Delete client/src/components directory

--- client/src/components/ui/sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils"
-
-export function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={cn("flex h-full flex-col gap-4 p-4")}>
-      {children}
-    </div>
-  )
-}
--- client/src/components/ui/tooltip.tsx
+++
@@ -1,9 +0,0 @@
-import {
-  Tooltip as TooltipPrimitive,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip"
-
-export const Tooltip = TooltipPrimitive
-export { TooltipTrigger, TooltipContent, TooltipProvider }
